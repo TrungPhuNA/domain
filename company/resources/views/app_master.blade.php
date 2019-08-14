9c71dbaafc0cbfad4ed5790ee93ff3fabb7b38db
TrungPhuNA <phupt.humg.94@gmail.com>
2019-08-14 17:19:09 +0700

    test  adada

--- company/resources/views/app_master.blade.php
+++ company/resources/views/app_master.blade.php
@@ -9,6 +9,7 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <link href="{{ mix('css/company.css') }}" rel="stylesheet">
+    <base href="/company/" />
 </head>
 <body>
 <div class="flex-center position-ref full-height">
