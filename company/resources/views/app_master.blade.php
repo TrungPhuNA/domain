f73be200459884a3f3914280d351ace22a55f3cb
TrungPhuNA <phupt.humg.94@gmail.com>
2019-08-14 14:55:26 +0700

    dd code

--- company/resources/views/app_master.blade.php
+++ company/resources/views/app_master.blade.php
@@ -8,8 +8,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <link rel="stylesheet" href="{{ asset('css/company.css') }}">
+    <link href="{{ mix('css/company.css') }}" rel="stylesheet">
 </head>
 <body>
 <div class="flex-center position-ref full-height">
